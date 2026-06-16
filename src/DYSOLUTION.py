fb660d69e8aec5f854b322c7707a47908ceddd0f
Znoywho <znoywho1508@gmail.com>
2026-06-16 20:38:45 +0700

    Covert to Frozenset

--- src/DYSOLUTION.py
+++ src/DYSOLUTION.py
@@ -1,7 +1,8 @@
 import numpy as np
-from typing import Dict, List, Set, Tuple
+from typing import Dict, FrozenSet, List, Set, Tuple
 from models import Order, Rack, NewInstance, State
 from instance import generate_instance, Paper_Example
+from simulation import items
 
 # TODO: The lower bound can be calculated by employing a MILP solver for **a set cover problem**
 
@@ -13,29 +14,74 @@ class Dynamic_solution:
         self.time_limit = time_limit
 
         self.capacity = self.inst.capacity
-        self.orders = set(o.id for o in self.inst.orders)
-        self.racks = set(r.id for r in self.inst.racks)
+        self.orders = frozenset(o.id for o in self.inst.orders)
+        self.racks = frozenset(r.id for r in self.inst.racks)
 
-        self.S = frozenset()  # SET  of states
-        self.gamma_hat = float("inf")  # best UPPER BOUND , INITIALLY = infinity,
+        self.heuristic = float("inf")  # best UPPER BOUND , INITIALLY = infinity,
         self.predoccess: Dict[int, State] = {}
+        self.genarated_states: Set[State] = set()
+        self.rack_visits_for_items: Dict[FrozenSet[int], int] = {}
+        self.gamma_hat = Dict[State, int]
 
-    def dynamic_programming(
+    def dynamic_programing(
         self,
-        state: State,  # (X, Y, Z)
+        state: State,  # (X^0 , Y^0 , Z^0, 𝛾^0)
         gamma,  # HELP to track suboptimal: If sub-solution is larger than upper bound -> stop
     ):
-        # if self.to_comparable(state) in self.S:
-        #     print("ALREADY CONTAINED IN SET")
-        # else:
-        #     print("NOT")
+        X, Y, Z = state
 
-        if state in self.S and gamma < self.gamma_hat:
-            print("GOING ON")
-            self.gamma_hat = gamma
+        if state in self.genarated_states and gamma >= self.heuristic:
+            return
+        # Ires  ← ∪o∈O⧵(X0 ∪Y 0 ) Io ∪ {i ∈ I|∃o ∈ Y ∶ (o, i) ∈ Z 0 }
+        # Set of Itemsm are not currently on rack or available
+        I_res = self.compute_missing_items(X, Y, Z)
 
-    def add_more(self, state):
-        self.S |= state
+        I_res_key = frozenset(I_res)
+        if I_res_key in self.rack_visits_for_items:
+            Gamma_I_res = self.rack_visits_for_items[I_res_key]
+        else:
+            rackVisit = self.compute_covering_set(I_res_key)
+            self.rack_visits_for_items[I_res_key] = rackVisit
+            Gamma_I_res = self.rack_visits_for_items[I_res_key]
+
+        if Gamma_I_res + gamma >= self.heuristic:
+            return
+
+        if state not in self.genarated_states:
+            self.genarated_states.add(state)
+
+        if self.orders == X:
+            self.heuristic = gamma
+            return
+
+    def compute_missing_items(self, X, Y, Z) -> Set[int]:
+        I_res = set()
+
+        for o in self.inst.orders:
+            if o.id not in X and o.id not in Y:
+                I_res |= o.items
+        for _, item_id in Z:
+            I_res.add(item_id)
+
+        return I_res
+
+    def compute_covering_set(self, ItemSet) -> int:
+        return 0
+
+    def stage1_rack(self, Z, rack_id):
+
+        Z_1: Set[Tuple[int, int]] = set()
+
+        for r in self.inst.racks:
+            if r.id == rack_id:
+                item = r.items
+                break
+
+        for order_id, item_id in Z:
+            if item_id not in item:
+                Z_1.add((order_id, item_id))
+
+        return Z_1
 
 
 ## TEST
@@ -55,8 +101,8 @@ if __name__ == "__main__":
     Y2 = frozenset([3, 2])
     Z2 = frozenset([(1, 1)])
 
-    sl.add_more((frozenset(X1), frozenset(Y1), frozenset(Z1)))
-
-    sl.dynamic_programming((X, Y, Z), 1000)
-    sl.dynamic_programming((X1, Y1, Z1), 1000)
-    sl.dynamic_programming((X2, Y2, Z2), 1000)
+    sl.genarated_states.add((frozenset(X1), frozenset(Y1), frozenset(Z1)))
+    #
+    # sl.dynamic_programing((X, Y, Z), 1000)
+    sl.dynamic_programing((X1, Y1, Z1), 1000)
+    # sl.dynamic_programing((X2, Y2, Z2), 1000)
